5039c3e3ba3eac23168f0d78265b3d4d10b2d90c
Mario Celis <celioso1@hotmail.com>
2023-09-08 18:44:56 -0500

    Request PUT

--- api/src/main/java/med/voll/api/medico/DatosListadoMedico.java
+++ api/src/main/java/med/voll/api/medico/DatosListadoMedico.java
@@ -1,8 +1,9 @@
 package med.voll.api.medico;
 
-public record DatosListadoMedico(String nombre, String especialidad, String telefono, String email) {
+public record DatosListadoMedico(Long id, String nombre, String especialidad, String telefono, String email) {
     public DatosListadoMedico(Medico medico){
-    this(medico.getNombre(),
+    this(medico.getId(),
+            medico.getNombre(),
             medico.getEspecialidad().toString(),
             medico.getTelefono(),
             medico.getEmail());
--- api/src/main/java/med/voll/api/pacientes/DatosListadoPaciente.java
+++ api/src/main/java/med/voll/api/pacientes/DatosListadoPaciente.java
@@ -2,9 +2,10 @@ package med.voll.api.pacientes;
 
 
 
-public record DatosListadoPaciente(String nombre, String seguro, String telefono, String email) {
+public record DatosListadoPaciente(Long id, String nombre, String seguro, String telefono, String email) {
     public DatosListadoPaciente(Paciente paciente){
-        this(paciente.getNombre(),
+        this(paciente.getId(),
+                paciente.getNombre(),
                 paciente.getSeguro(),
                 paciente.getTelefono(),
                 paciente.getEmail());
